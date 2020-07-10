ae1612a814da622db574f214807d48d3289b45f0
Christopher DeVito <christopher.devito@protonmail.com>
2020-07-10 11:49:29 -0400

    Completed ex1.py - tests passing

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,18 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    for weight in range(len(weights)):
+        remaining = limit - weights[weight]
+        if remaining in cache:
+            if weight > cache[remaining]:
+                print(f"({weight}, {cache[remaining]})")
+                return (weight, cache[remaining])
+            else:
+                print(f"({cache[remaining]}, {weight})")
+                return (cache[remaining], weight)
+
+        cache[weights[weight]] = weight
+
 
     return None
